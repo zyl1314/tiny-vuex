85b4fb109b40aa3eae972fb79d8dd8d8b9155687
zyl1314 <zhangyalong@qq.com>
2018-01-08 00:05:48 +0800

    v1.0

--- index.js
+++ index.js
@@ -1,29 +1,96 @@
 import mixin from './mixin.js';
-import observe from './observe.js';
+
+let Vue = null
 
 class Store {
-
   constructor(options) {
+    this._mutations = Object.create(null);
+    this._actions = Object.create(null);
+    this._vm = Object.create(null);
+
     let {
       state = {},
-      mutations = {}, 
+      getters = {},
+      mutations = {},
       actions = {}
     } = options;
+
+
+    // 数据响应
+    this.observeState(state, getters);
+    // 注册mutions
+    this.registerMutations(mutations);
+    // 注册actions
+    this.registerActions(actions);
+  }
+
+  get state() {
+    return this._vm.state
+  }
+
+  observeState(state, getters) {
+    let store = this;
+    store.getters = {};
+
+    // 注入state getters
+    let computed = {};
+    for (let key in getters) {
+      computed[key] = function() {
+        return getters[key](state, store.getters);
+      }
+    }
+    // 响应式
+    store._vm = new Vue({
+      data: { state },
+      computed
+    })
+    //  代理getters
+    for (let key in getters) {
+      Object.defineProperty(store.getters, key, {
+        get() {
+          return store._vm[key]
+        }
+      })      
+    }
+  }
+
+  registerMutations(mutations) {
+    let _mutations = this._mutations;
+    let state = this.state;
+
+    for (let type in mutations) {
+      _mutations[type] = function(payload) {
+        mutations[type](state, payload);
+      }
+    }
+  }
+
+  registerActions(actions) {
+    let _actions = this._actions;
+    let store = this;
+
+    for (let type in actions) {
+      _actions[type] = function(payload) {
+        actions[type](store, payload);
+      }
+    }
   }
 
   commit(type, payload) {
-
+    let handle = this._mutations[type];
+    handle && handle(payload);
   }
 
   dispatch(type, payload) {
-
+    let handle = this._actions[type];
+    handle && handle(payload);
   }
 }
 
-
-
-function install(Vue) {
-  mixin(Vue) 
+// 注册
+function install(_Vue) {
+  Vue = _Vue
+  mixin(Vue)
 }
 
 export default {
--- mixin.js
+++ mixin.js
@@ -1,23 +1,14 @@
-let activeInstance = null;
-
 export default function mixin(Vue) {
   Vue.mixin({
-    beforeCreate: mixinHook,
-    breforeUpdate: mixinResetInstance 
+    beforeCreate: mixinStore
   })
 }
 
-function mixinHook() {
-  const options = this.$options
+function mixinStore() {
+  let options = this.$options
   if (options.store) {
     this.$store = options.store
-  } else if (options.parent && options.parent.$store) {
+  } else if(options.parent && options.parent.$store){
     this.$store = options.parent.$store
-  }  
-
-  activeInstance = this;
-}
-
-function mixinResetInstance() {
-  activeInstance = this;
+  }
 }
